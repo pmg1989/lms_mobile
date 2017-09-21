fa3211378268d45a0c1bfbec842f72b873b3242a
pmg1989 <972401854@qq.com>
2017-09-21 12:20:42 +0800

    fix eslint

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,51 +1,19 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {bindActionCreators} from 'redux'
-import TaskForm from '../components/TaskForm'
-import TaskList from '../components/TaskList'
-import TaskStats from '../components/TaskStats'
-import selector from '../selector'
-import {taskAdd, taskDone, taskUndone, taskRemove, taskEdit, taskFilter} from '../actions'
-import './style.less'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
-const mapDispatchToProps = dispatch => ({
-  onTaskAdd: bindActionCreators(taskAdd, dispatch),
-  onTaskDone: bindActionCreators(taskDone, dispatch),
-  onTaskUndone: bindActionCreators(taskUndone, dispatch),
-  onTaskRemove: bindActionCreators(taskRemove, dispatch),
-  onTaskEdit: bindActionCreators(taskEdit, dispatch),
-  onTaskFilter: bindActionCreators(taskFilter, dispatch)
-})
-
-class App extends Component {
-
-  render() {
-    const { tasks, taskCount, doneTaskCount, filters, onTaskAdd, onTaskDone, onTaskUndone, onTaskRemove, onTaskEdit, onTaskFilter } = this.props
-
-    const taskListProps = {
-      tasks,
-      onTaskEdit,
-      onTaskDone,
-      onTaskUndone,
-      onTaskRemove
-    }
-
-    const taskStatsProps = {
-      filters,
-      taskCount,
-      doneTaskCount,
-      onTaskFilter
-    }
-
-    return (
-      <div id='viewport'>
-        <TaskForm onSave={onTaskAdd}/>
-        <TaskList {...taskListProps}/>
-        <TaskStats {...taskStatsProps}/>
-        {this.props.children}
-      </div>
-    )
-  }
+const App = ({ children }) => {
+  return (
+    <div>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {})
+      })}
+    </div>
+  )
 }
 
-export default connect(selector, mapDispatchToProps)(App)
+App.propTypes = {
+  children: PropTypes.element.isRequired,
+}
+
+export default connect()(App)
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,13 @@
 import React from 'react'
-import { IndexRoute, Route, Link, IndexRedirect, Redirect } from 'react-router'
+import { Route } from 'react-router'
 
 /* containers */
 import App from './containers/App'
-import Demo from './containers/Demo'
-import Demo2 from './containers/Demo2'
 
 
 const routes = (
-  <Route path='/' component={App}>
-    <Route path='/demo1' component={Demo} />
-    <Route path='/demo2' component={Demo2} />
+  <Route path="/" component={App}>
+    <Route path="/demo1" component={() => (<div>haha</div>)} />
   </Route>
 )
 
