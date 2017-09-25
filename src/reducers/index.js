ee056a0c89ee79bdf6456a79f2367d9c309e12af
pmg1989 <972401854@qq.com>
2017-09-25 17:28:10 +0800

    fix AudioPlayer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
+import { combineReducers } from 'redux-immutable'
 import routing from './route-reducers'
+
 import home from './home'
 
-export default {
+export default combineReducers({
   home,
   routing,
-}
+})
--- src/store.js
+++ src/store.js
@@ -1,12 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import { combineReducers } from 'redux-immutable'
+
 import thunk from 'redux-thunk'
 import Immutable from 'immutable'
 import reducers from './reducers'
 
-const reducer = combineReducers(reducers)
-
-const store = createStore(reducer,
+const store = createStore(reducers,
   Immutable.Map({}),
   compose(
     applyMiddleware(thunk),
