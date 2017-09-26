09cac5d1cbef15ef57e5d65198dd6d088dfd1211
pmg1989 <972401854@qq.com>
2017-09-26 12:52:25 +0800

    fix AudioPlayer

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -76,7 +76,7 @@ class AudioPlayer extends Component {
               <span className={styles.author}>{current.author}</span>
             </div>
           </div>
-          <div className={styles.right}>
+          <div className={styles.right} onClick={(e) => { e.stopPropagation() }}>
             {loop ? <Icon onClick={::this.handleLoop} type={require('svg/loop.svg')} /> : <Icon onClick={::this.handleLoop} type={require('svg/loop-no.svg')} />}
             {playing ? <Icon onClick={::this.handlePlayPause} type={require('svg/pause.svg')} /> : <Icon onClick={::this.handlePlayPause} type={require('svg/play.svg')} />}
             <Icon type={require('svg/next.svg')} onClick={::this.handleNext} />
