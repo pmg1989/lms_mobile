437acf4168b21de7945044037623a08360ea1265
pmg1989 <972401854@qq.com>
2017-09-27 18:38:06 +0800

    fix debug loading

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,46 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { PropTypes } from 'prop-types'
 import { connect } from 'react-redux'
 import { NavBar } from 'antd-mobile'
 import styles from './Header.less'
 
-class Header extends Component {
-  state = {
-    show: false,
+const Header = ({ dispatch, children, ...headerProps }) => {
+  const handleBack = () => {
+    // router.goBack()
   }
 
-  componentDidMount () {
-    setTimeout(() => {
-      this.setState({ show: true })
-    }, 100)
+  const navBarProps = {
+    leftContent: '返回',
+    iconName: `#${require('../../svg/back.svg').default.id}`,
+    mode: 'dark',
+    onLeftClick: handleBack,
+    ...headerProps,
   }
 
-  render () {
-    const { dispatch, children, ...headerProps } = this.props
-    const { show } = this.state
-
-    const handleBack = () => {
-      // router.goBack()
-    }
-
-    const navBarProps = {
-      leftContent: '返回',
-      iconName: `#${require('../../svg/back.svg').default.id}`,
-      mode: 'dark',
-      onLeftClick: handleBack,
-      ...headerProps,
-    }
-
-    return (
-      <div className={styles['fixed-top']}>
-        {show &&
-          <NavBar {...navBarProps}>
-            {children.length > 8 ? `${children.substr(0, 8)}...` : children}
-          </NavBar>
-        }
-      </div>
-    )
-  }
+  return (
+    <div className={styles['fixed-top']}>
+      <NavBar {...navBarProps}>
+        {children.length > 8 ? `${children.substr(0, 8)}...` : children}
+      </NavBar>
+    </div>
+  )
 }
 
 Header.propTypes = {
--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,41 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { PropTypes } from 'prop-types'
 import styles from './Icon.less'
 
-class Icon extends Component {
+const Icon = ({ className, style, type, onClick, ...props }) => {
+  return (
+    <span className={styles.icon_wrap} onClick={onClick}>
+      <svg className={`am-icon am-icon-${type.default ? type.default.id : type} ${className || ''}`} style={style} {...props}>
+        <use xlinkHref={`#${type.default ? type.default.id : type}`} />
+      </svg>
+    </span>
+  )
+}
 
-  static propTypes = {
-    className: PropTypes.string,
-    type: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.object,
-    ]).isRequired,
-    style: PropTypes.object,
-    onClick: PropTypes.func,
-  }
-
-  state = {
-    show: false,
-  }
-
-  componentDidMount () {
-    setTimeout(() => {
-      this.setState({ show: true })
-    }, 100)
-  }
-
-  render () {
-    const { className, style, type, onClick, ...props } = this.props
-    const { show } = this.state
-
-    return (
-      <span className={styles.icon_wrap} onClick={onClick}>
-        <svg className={`am-icon am-icon-${type.default ? type.default.id : type} ${className || ''}`} style={style} {...props}>
-          {show && <use xlinkHref={`#${type.default ? type.default.id : type}`} />}
-        </svg>
-      </span>
-    )
-  }
+Icon.propTypes = {
+  className: PropTypes.string,
+  type: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+  ]).isRequired,
+  style: PropTypes.object,
+  onClick: PropTypes.func,
 }
 
 export default Icon
