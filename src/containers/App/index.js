906857292039a1ab51f88bb0e04114bb230d0914
pmg1989 <972401854@qq.com>
2017-10-09 16:35:24 +0800

    fix App

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
 import { appActions } from 'actions'
 
@@ -9,16 +10,19 @@ class App extends Component {
     children: PropTypes.element.isRequired,
     location: PropTypes.object.isRequired,
     onApp: PropTypes.object.isRequired,
+    goTo: PropTypes.func.isRequired,
   }
 
   componentDidMount () {
-    const { location: { query: { mobile, token } }, onApp } = this.props
+    const { location: { query: { mobile, token } }, onApp, goTo } = this.props
     if (!!mobile && !!token) {
       onApp.authLogin(mobile, token).then((res) => {
-        console.log(res)
+        if (!res.app.authorized) {
+          goTo('/login')
+        }
       })
     } else {
-      console.log('to login')
+      goTo('/login')
     }
   }
 
@@ -41,6 +45,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   onApp: bindActionCreators(appActions, dispatch),
+  goTo: bindActionCreators(push, dispatch),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
