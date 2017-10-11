b8899cc49a59cff9ba6f9fe32613d4e4c600b74a
pmg1989 <972401854@qq.com>
2017-10-11 15:11:55 +0800

    fix Errot

--- src/containers/Error/index.js
+++ src/containers/Error/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { Header } from 'components'
+import { Header, Icon } from 'components'
+import styles from './Error.less'
 
 const Error = () => {
   return (
     <div className="content-box">
       <Header>页面找不到了</Header>
       <div className="content">
-        <p>抱歉，您访问的页面找不到了</p>
+        <div className={styles.box}>
+          <Icon className={styles.icon} type={require('svg/cry.svg')} />
+          <p>抱歉，您访问的页面找不到了</p>
+        </div>
       </div>
     </div>
   )
