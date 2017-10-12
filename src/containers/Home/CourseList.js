e15ff6f62ddfad789da2d33411bb1565ee0ad964
pmg1989 <972401854@qq.com>
2017-10-12 11:15:47 +0800

    fix courseList

--- src/containers/Home/CourseList.js
+++ src/containers/Home/CourseList.js
@@ -27,20 +27,47 @@ TitleBanner.propTypes = {
   status: PropTypes.number.isRequired,
 }
 
-const Course = ({ title, status, type, item }) => {
+const PFCourse = ({ status, item }) => {
   const hasNext = !!item.get('current_lesson_available')
   const courseType = item.get('category_idnumber')
   const isVip = courseType.includes('-vip-')
-  const linkDic = {
-    profession: `/progress/${courseType}/${encodeURIComponent(item.get('contractid'))}`,
-    hd: `/reserve/${courseType}/${encodeURIComponent(item.get('hdid'))}`,
-    jl: `/reserve/${courseType}/${encodeURIComponent(item.get('jlid'))}`,
-  }
+  const linkTo = `/progress/${courseType}/${encodeURIComponent(item.get('contractid'))}`
 
   return (
     <div className={styles.item}>
       <div className={styles.left}>
-        <span className={styles.title}>{title}</span>
+        <span className={styles.title}>专业课</span>
+        <span className={styles.content}>
+          {status === 0 && '待开课'}
+          {status === 1 && !hasNext && '下节课 未预约'}
+          {status === 1 && hasNext &&
+            <span>下节课 {moment.unix(item.get('current_lesson_available')).format('YYYY-MM-DD HH:mm')} (即将开课)</span>}
+          {status === 2 && '已结课'}
+        </span>
+      </div>
+      <div className={styles.right}>
+        <span>已完成 · {item.get('attended_lesson_cnt')} / {item.get('constract_lesson_cnt')}</span>
+        {status === 1 && isVip && <span><LinkToken className={classnames(styles.btn, styles.btn_blue)} to={linkTo}>预约</LinkToken></span>}
+        {((status === 1 && !isVip) || status === 2) && <span><LinkToken className={styles.btn} to={linkTo}>查看</LinkToken></span>}
+      </div>
+    </div>
+  )
+}
+PFCourse.propTypes = {
+  status: PropTypes.number.isRequired,
+  item: PropTypes.object.isRequired,
+}
+
+const HDCourse = ({ status, item }) => {
+  const hasNext = !!item.get('current_lesson_available')
+  const courseType = item.get('category_idnumber')
+  const isVip = courseType.includes('-vip-')
+  const linkTo = `/reserve/${courseType}/${encodeURIComponent(item.get('hdid'))}`
+
+  return (
+    <div className={styles.item}>
+      <div className={styles.left}>
+        <span className={styles.title}>互动课</span>
         <span className={styles.content}>
           {status === 0 && '待开课'}
           {status === 1 && !hasNext && '下节课 未预约'}
@@ -53,17 +80,48 @@ const Course = ({ title, status, type, item }) => {
       </div>
       <div className={styles.right}>
         <span>已完成 · {item.get('attended_lesson_cnt')} / {item.get('constract_lesson_cnt')}</span>
-        {status === 1 && isVip && <span><LinkToken className={classnames(styles.btn, styles.btn_blue)} to={linkDic[type]}>预约</LinkToken></span>}
-        {((status === 1 && !isVip) || status === 2) && <span><LinkToken className={styles.btn} to={linkDic[type]}>查看</LinkToken></span>}
+        {status === 1 && isVip && <span><LinkToken className={classnames(styles.btn, styles.btn_blue)} to={linkTo}>预约</LinkToken></span>}
+        {((status === 1 && !isVip) || status === 2) && <span><LinkToken className={styles.btn} to={linkTo}>查看</LinkToken></span>}
       </div>
     </div>
   )
 }
-Course.propTypes = {
-  title: PropTypes.string.isRequired,
+HDCourse.propTypes = {
   status: PropTypes.number.isRequired,
   item: PropTypes.object.isRequired,
-  type: PropTypes.string.isRequired,
+}
+
+const JLCourse = ({ status, item }) => {
+  const hasNext = !!item.get('current_lesson_available')
+  const courseType = item.get('category_idnumber')
+  const isVip = courseType.includes('-vip-')
+  const linkTo = `/reserve/${courseType}/${encodeURIComponent(item.get('jlid'))}`
+
+  return (
+    <div className={styles.item}>
+      <div className={styles.left}>
+        <span className={styles.title}>交流课</span>
+        <span className={styles.content}>
+          {status === 0 && '待开课'}
+          {status === 1 && !hasNext && '下节课 未预约'}
+          {status === 1 && hasNext &&
+            <span>下节课 {moment.unix(item.get('current_lesson_available')).format('YYYY-MM-DD HH:mm')} (即将开课)
+              {true && <Icon className={styles.icon_minus} type={require('svg/minus.svg')} />}
+            </span>}
+          {status === 2 && '已结课'}
+        </span>
+      </div>
+      <div className={styles.right}>
+        <span>已完成 · {item.get('attended_lesson_cnt')} / {item.get('constract_lesson_cnt')}</span>
+        {status === 1 && isVip && <span><LinkToken className={classnames(styles.btn, styles.btn_blue)} to={linkTo}>预约</LinkToken></span>}
+        {((status === 1 && !isVip) || status === 2) && <span><LinkToken className={styles.btn} to={linkTo}>查看</LinkToken></span>}
+      </div>
+    </div>
+  )
+}
+JLCourse.propTypes = {
+  status: PropTypes.number.isRequired,
+  item: PropTypes.object.isRequired,
 }
 
 const CourseList = ({ status, list }) => {
@@ -75,9 +133,9 @@ const CourseList = ({ status, list }) => {
         return (
           <div key={key} className={styles.list}>
             <TitleBanner title={item.getIn(['profession', 'category_summary'])} status={status} image={`./images/course-type/${courseType}.png`} />
-            <Course title="专业课" type={'profession'} status={status} item={item.get('profession')} />
-            {item.getIn(['hd', 'hdid']) && <Course type={'hd'} title="互动课" status={status} item={item.get('hd')} />}
-            {item.getIn(['jl', 'jlid']) && <Course type={'jl'} title="交流课" status={status} item={item.get('jl')} />}
+            <PFCourse status={status} item={item.get('profession')} />
+            {item.getIn(['hd', 'hdid']) && <HDCourse type={'hd'} title="互动课" status={status} item={item.get('hd')} />}
+            {item.getIn(['jl', 'jlid']) && <JLCourse type={'jl'} title="交流课" status={status} item={item.get('jl')} />}
             {status !== 0 &&
               <div className={styles.btn_box}>
                 <Link className={styles.btn} to={'/demo/123/456?name=felix&token=abc'}>
