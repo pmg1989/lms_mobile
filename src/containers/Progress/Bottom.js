a2a25b3ba0220e047cdee6b74633e4df0f56ea0d
pmg1989 <972401854@qq.com>
2017-10-16 18:03:34 +0800

    fix Progress

--- src/containers/Progress/Bottom.js
+++ src/containers/Progress/Bottom.js
@@ -2,13 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Immutable from 'immutable'
 import classnames from 'classnames'
+import moment from 'moment'
 import { browserHistory } from 'react-router'
 import { queryString } from 'utils/tools'
-import { Popover } from 'antd-mobile'
+import { Popover, Toast } from 'antd-mobile'
 import { Icon, LinkToken } from 'components'
 import styles from './Bottom.less'
 
 const Item = Popover.Item
+const now = new Date().getTime() / 1000
 
 const Bottom = ({ info, params: { type, courseType, contractId } }) => {
   const popoverProps = {
@@ -29,6 +31,34 @@ const Bottom = ({ info, params: { type, courseType, contractId } }) => {
     })
   }
 
+  const checkIsLock = (e) => {
+    const lockStart = info.get('contract_freezestart')
+    const lockEnd = info.get('contract_freezeend')
+
+    if(lockStart && lockEnd && (now >= lockStart && now <= lockEnd)) {
+      e.preventDefault()
+      Toast.info(`课程已冻结，无法订课！冻结时间为：${moment.unix(lockStart).format('YYYY-MM-DD')} ~ ${moment.unix(lockEnd).format('YYYY-MM-DD')}`)
+      return false
+    }
+    return true
+  }
+
+  const checkIsVip = (e) => {
+    if(checkIsLock(e)) {
+      const categoryIdnumber = info.get('category_idnumber')
+      const isVip = categoryIdnumber.includes('-vip-')
+      const notEnroll = ['composition', 'theory'].includes('categoryIdnumber')
+      if(type === 'profession' && !isVip) {
+        e.preventDefault()
+        Toast.info('只有VIP学员可以预约上课哦！')
+      }
+      if(type === 'profession' && notEnroll) {
+        e.preventDefault()
+        Toast.info('该课程暂时无法预约哦!')
+      }
+    }
+  }
+
   return (
     <div className={styles.box}>
       <div className={styles.left} onClick={goToFeedBack}>
@@ -39,17 +69,17 @@ const Bottom = ({ info, params: { type, courseType, contractId } }) => {
       </div>
       {['profession', 'jl'].includes(type) &&
         <div className={styles.right}>
-          <LinkToken className={styles.btn} to={`/reserve/${courseType}/${contractId}`}>
+          <LinkToken className={styles.btn} onClick={checkIsVip} to={`/reserve/${courseType}/${contractId}`}>
             预约课程
           </LinkToken>
         </div>
       }
       {type === 'hd' &&
         <div className={styles.right}>
-          <LinkToken className={styles.btn} to={`/reserve/hd-rhythm/${contractId}`}>
+          <LinkToken className={styles.btn} onClick={checkIsLock} to={`/reserve/hd-rhythm/${contractId}`}>
             预约节奏课
           </LinkToken>
-          <LinkToken className={classnames(styles.btn, styles.orange)} to={`/reserve/hd-yoga/${contractId}`}>
+          <LinkToken className={classnames(styles.btn, styles.orange)} onClick={checkIsLock} to={`/reserve/hd-yoga/${contractId}`}>
             预约瑜伽课
           </LinkToken>
         </div>
