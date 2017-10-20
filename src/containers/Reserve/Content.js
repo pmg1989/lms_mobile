e10f033cf77bed7e8e6f65ac589504fcc775a146
pmg1989 <972401854@qq.com>
2017-10-20 17:06:38 +0800

    fix Reserve.js

--- src/containers/Reserve/Content.js
+++ src/containers/Reserve/Content.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Immutable from 'immutable'
+// import { Picker } from 'antd-mobile'
 import { Calendar, Icon } from 'components'
-// import moment from 'moment'
 import styles from './Content.less'
+
+const DateSelect = () => {
+  return (
+    <div className={styles.date_select}>
+      <Icon type={require('svg/complete.svg')} />
+      <span className={styles.center}>选择时间</span>
+      <Icon type={require('svg/arrow-down.svg')} />
+    </div>
+  )
+}
 
 const Content = ({ dayOfLessons, info }) => {
   const calendarProps = {
@@ -17,11 +27,7 @@ const Content = ({ dayOfLessons, info }) => {
     <div className={styles.box}>
       <div className={styles.title}>预约第{info.get('attendedlesson_cnt')}课</div>
       <Calendar {...calendarProps} />
-      <div className={styles.date_select}>
-        <Icon type={require('svg/complete.svg')} />
-        <span className={styles.center}>选择时间</span>
-        <Icon type={require('svg/arrow-down.svg')} />
-      </div>
+      <DateSelect />
       <div className={styles.tips}>
         已约课<span>5</span>人 限30人
       </div>
