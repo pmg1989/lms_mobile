357ad18bd5d8371b17553b0959696a15eb42d929
pmg1989 <972401854@qq.com>
2017-10-24 12:14:18 +0800

    fix Progress

--- src/containers/Progress/Bottom.js
+++ src/containers/Progress/Bottom.js
@@ -48,6 +48,8 @@ const Bottom = ({ info, params: { type, categoryId, contractId } }) => {
       const categoryIdnumber = info.get('category_idnumber')
       const isVip = categoryIdnumber.includes('-vip-')
       const notEnroll = ['composition', 'theory'].includes('categoryId')
+      const curCnt = info.get('attendedlesson_cnt')
+      const countCnt = info.get('contractlesson_cnt')
       if (type === 'profession' && !isVip) {
         e.preventDefault()
         Toast.info('只有VIP学员可以预约上课哦！')
@@ -56,7 +58,36 @@ const Bottom = ({ info, params: { type, categoryId, contractId } }) => {
         e.preventDefault()
         Toast.info('该课程暂时无法预约哦!')
       }
+      if(curCnt === countCnt) {
+        e.preventDefault()
+        Toast.info('你的课程已经预约满了哦!')
+      }
     }
+  }
+
+  const isDisabled = () => {
+    const lockStart = info.get('contract_freezestart')
+    const lockEnd = info.get('contract_freezeend')
+    if (lockStart && lockEnd && (now >= lockStart && now <= lockEnd)) {
+      return true // 账号被锁了
+    }
+
+    const categoryIdnumber = info.get('category_idnumber')
+    const isVip = categoryIdnumber.includes('-vip-')
+    const notEnroll = ['composition', 'theory'].includes('categoryId')
+    if (type === 'profession' && !isVip) {
+      return true // 只有VIP学员可以预约上课
+    }
+    if (type === 'profession' && notEnroll) {
+      return true // composition、theory课程暂时无法预约
+    }
+
+    const curCnt = info.get('attendedlesson_cnt')
+    const countCnt = info.get('contractlesson_cnt')
+    if(curCnt === countCnt) {
+      return true // 课程已经预约满了
+    }
+    return false
   }
 
   return (
@@ -69,17 +100,17 @@ const Bottom = ({ info, params: { type, categoryId, contractId } }) => {
       </div>
       {['profession', 'jl'].includes(type) &&
         <div className={styles.right}>
-          <LinkToken className={styles.btn} onClick={checkIsVip} to={`/reserve/${encodeURIComponent(contractId)}/${categoryId}`}>
+          <LinkToken className={classnames(styles.btn, isDisabled() && styles.disabled)} onClick={checkIsVip} to={`/reserve/${encodeURIComponent(contractId)}/${categoryId}`}>
             预约课程
           </LinkToken>
         </div>
       }
       {type === 'hd' &&
         <div className={styles.right}>
-          <LinkToken className={styles.btn} onClick={checkIsLock} to={`/reserve/${encodeURIComponent(contractId)}/hd-rhythm`}>
+          <LinkToken className={classnames(styles.btn, isDisabled() && styles.disabled)} onClick={checkIsLock} to={`/reserve/${encodeURIComponent(contractId)}/hd-rhythm`}>
             预约节奏课
           </LinkToken>
-          <LinkToken className={classnames(styles.btn, styles.disabled)} onClick={checkIsLock} to={`/reserve/${encodeURIComponent(contractId)}/hd-yoga`}>
+          <LinkToken className={classnames(styles.btn, isDisabled() && styles.disabled)} onClick={checkIsLock} to={`/reserve/${encodeURIComponent(contractId)}/hd-yoga`}>
             预约瑜伽课
           </LinkToken>
         </div>
