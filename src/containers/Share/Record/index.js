47b90c98ef5eabf0de3dcb8c3bb2fcf7ef03ae8c
pmg1989 <972401854@qq.com>
2017-10-31 16:49:38 +0800

    fix share

--- src/containers/Share/Record/index.js
+++ src/containers/Share/Record/index.js
@@ -1,28 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Header } from 'components'
+import wechat from 'utils/wechat'
 import Content from './Content'
 
-const Record = ({ location }) => {
-  const { pathname, query, search } = location
-  console.log(pathname, query, search, window.location.origin)
-  const headerProps = {
-    leftContent: null,
-    iconName: null,
+class Record extends Component {
+  static propTypes = {
+    location: PropTypes.object.isRequired,
   }
 
-  return (
-    <div className="content-box">
-      <Header {...headerProps}>牛班音乐学校</Header>
-      <div className="content">
-        <Content query={query} />
-      </div>
-    </div>
-  )
-}
+  componentWillMount() {
+    const { query: { title, image, source } } = this.props.location
+    wechat.share({
+      title,
+      desc: '我录了一首歌曲。',
+      imgUrl: image,
+      type: 'music',
+      dataUrl: source,
+    })
+  }
 
-Record.propTypes = {
-  location: PropTypes.object.isRequired,
+  render () {
+    const { query } = this.props.location
+    const headerProps = {
+      leftContent: null,
+      iconName: null,
+    }
+
+    return (
+      <div className="content-box">
+        <Header {...headerProps}>牛班音乐学校</Header>
+        <div className="content">
+          <Content query={query} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Record
