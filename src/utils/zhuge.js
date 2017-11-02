b2e57be8d20fc52a96e7199686fd67ea5c9cc6ae
pmg1989 <972401854@qq.com>
2017-11-02 16:01:28 +0800

    fix zhugeIO

--- src/utils/zhuge.js
+++ src/utils/zhuge.js
@@ -2,7 +2,7 @@ import { isIOS } from 'utils/app'
 
 export function ioShare (title, author) {
   const type = isIOS() ? 'ios' : 'android'
-  
+
   zhuge.track('分享我的录音', {
     歌曲名称: title,
     原唱: author,
